e437bed95d3e9027890f136cc598b7c5c1f6ac7f
Igor Storozhev <igor.storozhev@gmail.com>
2014-01-13 15:07:42 +0400

    Commiting before pull

--- HeadFirst/nester.py
+++ HeadFirst/nester.py
@@ -1,6 +1,6 @@
 """This is the "neter.py" module, and it provides one function called
         print_lol() wich prints list that may or may not include nested lists"""
-def print_lol(the_list):
+def print_lol(the_list, tab = 0):
         """This function takes a positional arguments called "the list", which is an
                 Python list (of, possibly, nested lists). Each data item in the provided list
                 is (recursively) printed to screen on its own line"""
@@ -8,4 +8,7 @@ def print_lol(the_list):
                 if isinstance(each_item,list) :
                         print_lol(each_item)
                 else:
-                        print(each_item)
+                	if tab > 0 :
+                		for num in range(tab):
+                			print("\t", end='')
+                	print(each_item)
