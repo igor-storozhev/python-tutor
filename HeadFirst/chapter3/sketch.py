b64f46c5f129a235975f565c7d1b82f2f0d05102
Igor Storozhev <igor.storozhev@gmail.com>
2014-01-16 18:39:02 +0400

    modify try-except

--- HeadFirst/chapter3/sketch.py
+++ HeadFirst/chapter3/sketch.py
@@ -1,8 +1,15 @@
-data =  open('sketch.txt')
-for each_line in data:
-	if not each_line.find(':') == -1 :
-		(role, line_spoken) = each_line.split(':',1)
-		print(role, end='')
-		print(' said: ', end = '')
-		print(line_spoken, end='')
-data.close()
+try:
+	data =  open('sketch.txt')
+
+	for each_line in data:
+		if not each_line.find(':') == -1 :
+			try:
+				(role, line_spoken) = each_line.split(':',1)
+				print(role, end='')
+				print(' said: ', end = '')
+				print(line_spoken, end='')
+			except:
+				pass
+	data.close()
+except:
+	print('The data file is missing')
